7492cb22eadbc88d063e6a54a1a0041ce70896a0
rishabhguptajs <rishabhgupta4523@gmail.com>
2025-03-08 22:02:14 +0530

    Implement vector embedding and database ingestion for web scraping

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import axios from "axios";
 import * as cheerio from 'cheerio';
+import dotenv from 'dotenv';
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { TaskType } from "@google/generative-ai";
+import { ChromaClient } from "chromadb";
+
+dotenv.config();
+
+const genai = new GoogleGenerativeAIEmbeddings({
+    apiKey: process.env.GEMINI_API_KEY,
+    model: "text-embedding-004",
+    taskType: TaskType.RETRIEVAL_DOCUMENT
+})
+
+const chromaClient = new ChromaClient({ 
+    path: 'http://localhost:8000'
+})
+chromaClient.heartbeat();
+
+const WEB_COLLECTION = `WEB_SCRAPED_DATA_COLLECTION-1`
 
 async function scrapeWebpage(url){
     const { data } = await axios.get(url);
@@ -33,6 +52,55 @@ async function scrapeWebpage(url){
     }
 }
 
-async function generateVectorEmbeddings(){}
+async function generateVectorEmbeddings({ text }){
+    const embeddings = await genai.embedDocuments([text]);
+
+    return embeddings[0];
+}
+
+async function insertIntoDB({ embedding, url, body = '', head = '' }){
+    const collection = await chromaClient.getOrCreateCollection({
+        name: WEB_COLLECTION,
+    });
+
+    collection.add({
+        ids: [url],
+        embeddings: [embedding],
+        metadatas: [{ url, body, head }],
+    });
+}
+
+function chunkText(text, size) {
+    if (typeof text !== "string" || typeof size !== "number" || size <= 0) {
+        throw new Error("Invalid input: text must be a string and size must be a positive number.");
+    }
+    
+    let chunks = [];
+    for (let i = 0; i < text.length; i += size) {
+        chunks.push(text.substring(i, i + size));
+    }
+    
+    return chunks;
+}
+
+async function ingest(url = ''){
+    console.log(`Ingesting ${url}`);
+    const { head, body, internalLinks } = await scrapeWebpage(url);
+
+    const headEmbedding = await generateVectorEmbeddings({ text: head });
+    await insertIntoDB({ embedding: headEmbedding, url });
+
+    const bodyChunks = chunkText(body, 2000);
+
+    for(const chunk of bodyChunks){
+        const bodyEmbedding = await generateVectorEmbeddings({ text: chunk });
+        await insertIntoDB({ embedding: bodyEmbedding, url, head, body: chunk });
+    }
+
+    for(const link of internalLinks){
+        const _url = `${url}${link}`;
+        await ingest()
+    }
+}
 
 scrapeWebpage('https://rishabhguptajs.vercel.app').then(console.log)
